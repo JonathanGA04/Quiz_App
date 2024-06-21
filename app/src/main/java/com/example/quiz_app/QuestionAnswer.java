afbc1707588d3b7cfbda29e9358a8d3804e249d9
JonathanGA04 <gonzalezjthn@gmail.com>
2024-06-21 16:07:19 -0600

    Quiz App Inicialice una aplicacion en Android Studio utilizando java la cual es un quiz de 5 preguntas, este te dira tu score de esas 5 preguntas. en activity_main.xml le di el diseño de mi aplicacion utilizando 5 botones, 4 para las preguntas y 1 para enviar la respuesta.

---
+++ app/src/main/java/com/example/quiz_app/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "What is 10+26 ?",
+            "Who invented Telephone?",
+            "what is 12*9 ?",
+            "Who won the 2022 World Cup",
+            "In the given option, which is the Example of System Software?"
+    };
+    public static  String choice [][]={
+            {"32" , "42" , "36" , "38"},
+            {"Graham Bell" , "Einstein" , "Edison" , "None of the above"},
+            {"96" , "84" , "102" , "108"},
+            {"Portugal", "Argentina" , "Mexico" , "Brazil"},
+            {"Windows" , "Linux" , "MacOS" , "All of the above"}
+    };
+    public static String correctAnswers [] ={
+            "36",
+            "Graham Bell",
+            "108",
+            "Argentina",
+            "All of the above"};
+}
